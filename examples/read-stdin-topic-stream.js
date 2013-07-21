898f6ea078f6ff9a3e084230f5e1676fb75206b8
Mark Wolfe <mark@wolfe.id.au>
2013-07-21 12:22:01 +1000

    Simple example.

--- examples/read-stdin-topic-stream.js
+++ examples/read-stdin-topic-stream.js
@@ -1,9 +1,17 @@
 "use strict";
+/*
+ * This example is designed to be run from the command line as follows.
+ *
+ * cat /var/log/syslog | node examples/read-stdin-topic-stream.js
+ */
 
 var es = require('event-stream')
 var amqp = require('amqp')
 var topicStream = require('../lib/topic-stream.js')
 var log = require('debug')('read-stdin-topic-stream')
+
+// exit once we have processed the data that was piped in.
+process.stdin.on('close', process.exit)
 
 var connection =
   amqp.createConnection({url: "amqp://guest:guest@localhost:5672"})
@@ -13,6 +21,6 @@ connection.on('ready', function () {
 
   topicStream({connection: connection, exchangeName: '/events/input', routingKey: '#'}, function (err, ts) {
     log('topicStream', 'open')
-    es.pipeline(process.openStdin(), es.split(), ts)
+    es.pipeline(process.stdin, es.split(), ts)
   })
 })
